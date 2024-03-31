94d212bf73506133160439b469d702bfee03306b
vlyoung <159109188+vlyoung@users.noreply.github.com>
2024-03-31 14:00:09 -0700

    Delete brandnav directory

--- brandnav/menu.js
+++
@@ -1,7 +0,0 @@
-function showmobilemenu(){
-	document.getElementById("mobilemenuoverlay").style.display="block";}
-
-function hidemobilemenu(){
-	document.getElementById("mobilemenuoverlay").style.display="none";}
-
-// JavaScript Document
